6d3935fc77becb7c952bfa577776348a1d3a457d
longho <hoduylong411@gmail.com>
2020-12-13 21:42:12 +0700

    Commit - 13.12.2020

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
 
 class Movies extends Component {
   state = {
@@ -27,6 +28,7 @@ class Movies extends Component {
               <th>Stock</th>
               <th>Rate</th>
               <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -36,6 +38,9 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like liked={movie.liked} />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
