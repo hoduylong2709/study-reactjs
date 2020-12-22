bc8a8d19ebedf4f4e1d95f4b9195339759585280
longho <hoduylong411@gmail.com>
2020-12-22 11:24:41 +0700

    Commit - 22.12.2020

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
-import ProductDetails from "./components/productDetails";
-import NotFound from "./components/notFound";
 import "./App.css";
+import ProductDetails from './components/productDetails';
+import NotFound from './components/notFound';
 
 class App extends Component {
   render() {
@@ -16,10 +16,14 @@ class App extends Component {
         <NavBar />
         <div className='content'>
           <Switch>
+            <Route path='/products/:id' component={ProductDetails} />
             <Route path="/products" component={Products} />
-            <Route path="/posts" component={Posts} />
+            <Route path="/posts/:year?/:month?" component={Posts} />
             <Route path="/admin" component={Dashboard} />
-            <Route path="/" component={Home} />
+            <Redirect from='/messages' to='/posts' />
+            <Route path='/not-found' component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to='/not-found' />
           </Switch>
         </div>
       </div>
