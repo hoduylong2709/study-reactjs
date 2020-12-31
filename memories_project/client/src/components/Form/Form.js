d11ab28c0353794c08025056b4a938fb80b7e51a
longho <hoduylong411@gmail.com>
2020-12-31 21:30:05 +0700

    Commit - 31.12.2020

---
+++ memories_project/client/src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import useStyles from './styles';
+
+function Form(props) {
+  const classes = useStyles();
+  return (
+    <h1>FORM</h1>
+  );
+}
+
+export default Form;
