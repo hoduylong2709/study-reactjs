97fbafff7950ac05a3ce222af31ffcb2f06b9fc6
longho <hoduylong411@gmail.com>
2021-01-02 21:50:59 +0700

    Commit - 2.1.2021

--- memories_project/client/src/App.js
+++ memories_project/client/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import useStyles from './styles';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
 
 function App(props) {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <Container maxidth='lg'>
