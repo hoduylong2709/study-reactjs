28321d92a9bebd1e705b89b6be7b8d861022a3b8
longho <hoduylong411@gmail.com>
2021-01-04 17:36:27 +0700

    make it better

--- memories_project/server/index.js
+++ memories_project/server/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
@@ -12,10 +14,13 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://hoduylong:hoduylong123@cluster0.ktqou.mongodb.net/<dbname>?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+  res.send('Hello to Memories API');
+});
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((err) => console.log(error.message));
 
